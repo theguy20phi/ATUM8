cf309af096d78399605aa05a4ed9b4b7e00d9767
thomasdang92 <thomasdang92@gmail.com>
2023-03-05 19:48:50 -0600

    Wrote Red Auton.

--- 15in/src/atum8/autonRoutes/redAuton.cpp
+++ 15in/src/atum8/autonRoutes/redAuton.cpp
@@ -7,5 +7,19 @@
 #include "main.h"
 
 namespace atum8 {
-void redAuton() {}
+void redAuton() {
+  atum8::Drive drive;
+  atum8::Catapult catapult;
+  atum8::Intake intake;
+  atum8::EndGame endGame;
+
+   drive.move(-5, 200, 1000, false, 1);
+   intake.setRollerToRed();
+   drive.move(5, 200, 1000, false, 1);
+   drive.turn(-100, 200, 1000, 2);
+   intake.in();
+   drive.move(-49, 200, 1000, false, 2);
+   drive.move(-7, 50, 1000, false, 1);
+   drive.move(10, 200, 1000, false, 1);
+}
 } // namespace atum8
