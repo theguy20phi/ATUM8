04d62b2f6925814fe99151452e3a3be4dc03bb06
thomasdang92 <thomasdang92@gmail.com>
2023-03-05 19:49:19 -0600

    Wrote Auton for testing.

--- 15in/src/atum8/autonRoutes/testingAuton.cpp
+++ 15in/src/atum8/autonRoutes/testingAuton.cpp
@@ -7,5 +7,18 @@
 #include "main.h"
 
 namespace atum8 {
-void testingAuton() {}
+void testingAuton() {
+  atum8::Drive drive; 
+  atum8::Catapult catapult;
+  atum8::Intake intake;
+  atum8::EndGame endGame;
+
+   intake.setRollerToRed();
+   drive.move(5, 200, 1000, false, 1);
+   drive.turn(-100, 200, 1000, 2);
+   intake.in();
+   drive.move(-49, 200, 1000, false, 2);
+   drive.move(-7, 50, 1000, false, 1);
+   drive.move(10, 200, 1000, false, 1);
+}
 } // namespace atum8
