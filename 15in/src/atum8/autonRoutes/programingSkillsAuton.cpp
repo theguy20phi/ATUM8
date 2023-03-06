3568d5a13b607e0de89fab3c5d2e0aff3dd8fe30
thomasdang92 <thomasdang92@gmail.com>
2023-03-05 19:48:34 -0600

    Adjusted Programming Sills Auton.

--- 15in/src/atum8/autonRoutes/programingSkillsAuton.cpp
+++ 15in/src/atum8/autonRoutes/programingSkillsAuton.cpp
@@ -15,45 +15,46 @@ void programingSkillsAuton() {
   atum8::EndGame endGame;
 
   // Align with roller 1
-  drive.move(-23.2, 200, 1000, false, 1);
+  drive.move(-25, 200, 1000, false, 1);
   drive.turn(90, 200, 1000, 1);
-  drive.move(-4.7, 200, 1000, false, 1);
+  drive.move(-7, 200, 1000, false, 1);
 
   // Toggle roller 1
   intake.setRollerToRed();
 
   // Align with disk
-  drive.move(7, 200, 1000, false, 1);
-  drive.turn(-137.5, 200, 1000, 1);
+  drive.move(6, 200, 1000, false, 1);
+  drive.turn(-137, 200, 1000, 1);
 
   // Eat disk
   intake.in();
-  drive.move(-23, 100, 1000, false, 3);
+  drive.move(-23.5, 100, 1000, false, 3);
 
   // Align with roller 2
   drive.turn(45, 200, 1000, 1);
+  pros::delay(500);
   intake.stop();
-  drive.move(-6, 200, 1000, false, 1);
+  drive.move(-7, 200, 1000, false, 1);
 
   // Toggle roller 2
   intake.setRollerToRed();
 
   // Align with red goal
-  drive.move(3.7, 200, 1000, false, 1);
+  drive.move(5, 200, 1000, false, 1);
   intake.in();
   drive.turn(90, 200, 1000, 1);
-  drive.move(57, 200, 1000, false, 2);
+  drive.move(56, 200, 1000, false, 2);
 
   // Adjust to goal
-  drive.turn(-10, 200, 2000, .5);
+  drive.turn(-9, 200, 2000, .5);
 
   // Shoot Disks into red goal
   catapult.shoot();
   intake.stop();
 
   // Drive back towards 3 stack of disks
-  drive.turn(12, 200, 2000, .5);
-  drive.move(-29, 200, 1000, false, 2);
+  drive.turn(11, 200, 2000, .5);
+  drive.move(-28, 200, 1000, false, 2);
   drive.turn(92, 200, 2000, 1);
 
   // Hit wall to readjust robot part 1
@@ -66,7 +67,7 @@ void programingSkillsAuton() {
   pros::delay(500);
 
   // Hit wall to readjust robot part 2
-  drive.move(35, 200, 1000, false, 1.25);
+  drive.move(35, 150, 1000, false, 2);
 
   // Align with red goal part 2
   drive.move(-4, 200, 1000, false, 1);
@@ -95,7 +96,7 @@ void programingSkillsAuton() {
   // Eat 3 stack of disks part 2
   intake.in();
   drive.move(-20, 200, 1000, false, 1);
-  drive.move(-30, 40, 1000, false, 3);
+  drive.move(-29.5, 40, 1000, false, 3);
 
   // Align with blue goal
   drive.move(-14, 200, 1000, false, 1);
@@ -103,14 +104,14 @@ void programingSkillsAuton() {
   drive.move(31, 200, 1000, false, 1);
 
   // Adjust to goal
-  drive.turn(14, 200, 2000, .5);
+  drive.turn(10, 200, 2000, .5);
 
   // Shoot 3 stack of disks into blue goal
   catapult.shoot();
   intake.stop();
 
   // Align with row of 3 disks
-  drive.turn(-15, 200, 2000, 1);
+  drive.turn(-11, 200, 2000, 1);
   drive.move(-52, 200, 1000, false, 2);
   drive.turn(-135, 200, 2000, 2);
 
@@ -120,31 +121,23 @@ void programingSkillsAuton() {
   drive.move(-49, 100, 1000, false, 4);
 
   // Adjust to blue goal part 2
+  drive.turn(95, 200, 2000, 2);
+  //drive.move(5, 100, 1000, false, 1);
+
+
+  // Shoot row of 3 disks into blue goal
+  catapult.shoot();
+  intake.stop();
+
+  // Align bot for endgame
   drive.turn(90, 200, 2000, 1);
+  drive.move(-60, 200, 1000, false, 2);
+  drive.turn(9, 200, 2000, 1);
 
-  // Shoot row of 3 disks into blue goal
-  catapult.shoot();
-  intake.stop();
-
-  // Align with disks along goal
-  drive.turn(140, 200, 2000, 1);
-  intake.in();
-  drive.move(-47, 60, 1000, false, 3);
-  drive.turn(-80, 200, 2000, 1);
-
-  pros::delay(1000);
-
-  // Shoot row of 3 disks into blue goal
-  catapult.shoot();
-  catapult.stop();
-  intake.stop();
-
-  drive.turn(5, 2000, 200, 1);
-
-  drive.move(-26, 2000, 1000, false, 1);
-  drive.turn(80, 2000, 200, 1);
-  drive.move(-10, 50, 1000, false, 2);
+  // Wait for Braden's bot
+  pros::delay(2000);
 
   endGame.shoot();
+  catapult.down();
 }
 } // namespace atum8
