7dc8013f2e1710aeb4f5b1e6ee0a6e5e77fb11a3
thomasdang92 <thomasdang92@gmail.com>
2023-03-16 10:34:02 -0500

    Testing PID turn method.

--- 15in Worlds/src/main.cpp
+++ 15in Worlds/src/main.cpp
@@ -8,7 +8,26 @@
 #include "atum8\systems\intake.hpp"
 
 
-void initialize() {}
+void initialize() {
+	  pros::lcd::initialize();
+  pros::lcd::set_background_color(0, 0, 0);
+  pros::lcd::set_text_color(255, 255, 255);
+
+  // Enable Optical Sensors
+  atum8::opticalSensor.set_led_pwm(100);
+  atum8::opticalSensor.disable_gesture();
+
+  // Calibrate Inertial Sensors
+  atum8::imuSensorAlpha.reset();
+  atum8::imuSensorBeta.reset();
+  atum8::imuSensorCharlie.reset();
+  while (atum8::imuSensorAlpha.is_calibrating() ||
+         atum8::imuSensorBeta.is_calibrating() ||
+         atum8::imuSensorCharlie.is_calibrating()) {
+    pros::lcd::set_text(1, "IMUs ARE CALIBRATING DON'T TOUCH!!!");
+    pros::delay(100);
+  }
+}
 
 void disabled() {}
 
@@ -23,4 +42,5 @@ void opcontrol() {
 	drive.move(24, 200, 600, false, 1);
 	drive.move(-10, 200, 600, false, 1);
 	drive.move(-5, 200, 600, false, 1);
+	drive.turn(90, 200, 600, 1);
 }
