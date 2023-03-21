f55af5f25447646a15c3b2e8aa2fec5d5c050e36
thomasdang92 <thomasdang92@gmail.com>
2023-03-21 13:33:48 -0500

    Protected section added to class. Read desc.
    The setRightPower, setLeftPower, and setDriveBrakeMode methods were added into a protected section to that child classes the inherit the drive class can use them.

--- 15in Worlds/include/atum8/systems/drive.hpp
+++ 15in Worlds/include/atum8/systems/drive.hpp
@@ -1,42 +1,44 @@
 /**
  * @file drive.hpp
  * @author Thomas Tran Dang (thomasdang92@gmail.com)
- * @brief This file provides a class for all of the drive train methods for the 15in. 
-          As of the moment the 15in is a tank drive. 
- * @version 0.1
+ * @brief This file provides a class for all of the drive train methods for the
+ 15in. As of the moment the 15in is a tank drive.
+ * @version 0.2
  * @date 2023-03-14
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #pragma once
-#include "main.h"
 #include "atum8/controllers/pid.hpp"
-#include "atum8/slewRate.hpp"
 #include "atum8/imus.hpp"
+#include "atum8/slewRate.hpp"
+#include "main.h"
+
 
 namespace atum8 {
-    class Drive : Pid, SlewRate, Imus{
-        public:
-          void controller();
-          void move(double inches, double rpm, double acceleration, bool dift, double secThreshold);
-          void turn(double angle, double rpm, double acceleration, double secThreshold);
+class Drive : Pid, SlewRate, Imus {
+public:
+  void controller();
+  void move(double inches, double rpm, double acceleration, bool dift,
+            double secThreshold);
+  void turn(double angle, double rpm, double acceleration, double secThreshold);
+  double getRightPower();
+  double getLeftPower();
+  double getRightEncoderValues();
+  double getLeftEncoderValues();
+  double getEncoderAverages();
 
-        protected: 
-        void setRightPower(double power);
-        void setLeftPower(double power);
-        void setDriveBrakeMode(const std::string brakeMode);
+protected:
+  void setRightPower(double power);
+  void setLeftPower(double power);
+  void setDriveBrakeMode(const std::string brakeMode);
 
-        private:
-        double getRightPower();
-        double getLeftPower();
-        double getRightEncoderValues();
-        double getLeftEncoderValues();
-        double getEncoderAverages();
-        void resetEncoders();
-        void reset();
-        double msCounter;
-        double power;
-    };
-}
+private:
+  void resetEncoders();
+  void reset();
+  double msCounter;
+  double power;
+};
+} // namespace atum8
