d730ea0a8460024845e3e7cffcb44c05ce1ea971
thomasdang92 <thomasdang92@gmail.com>
2023-03-27 17:13:53 -0500

    Started new auton selector.

---
+++ 15in Worlds/include/atum8/autonSelector.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file  autonSelector.hpp
+ * @author Thomas Tran Dang (thomasdang92@gmail.com)
+ * @brief This file provides the class for the autonomous selector on the V5 Brain.
+ * @version 0.1
+ * @date 2023-03-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+#include "globals.hpp"
+#include "main.h"
+
+namespace atum8 {
+class AutonSelector {
+public:
+    void displayScreen();
+};
+}
--- 15in Worlds/src/main.cpp
+++ 15in Worlds/src/main.cpp
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "atum8/vision.hpp"
+#include "atum8/sensors/vision.hpp"
 #include "main.h"
 //#include "atum8\autonSelector.hpp"
 #include "atum8\systems\catapult.hpp"
@@ -7,27 +7,30 @@
 #include "atum8\systems\endGame.hpp"
 #include "atum8\globals.hpp"
 #include "atum8\systems\intake.hpp"
+#include "pros/colors.h"
+#include "pros/rtos.hpp"
+#include "pros/screen.hpp"
 
 
 void initialize() {
-	  pros::lcd::initialize();
-  pros::lcd::set_background_color(0, 0, 0);
-  pros::lcd::set_text_color(255, 255, 255);
+	  //pros::lcd::initialize();
+  //pros::lcd::set_background_color(0, 0, 0);
+  //pros::lcd::set_text_color(255, 255, 255);
 
   // Enable Optical Sensors
-  atum8::opticalSensor.set_led_pwm(100);
-  atum8::opticalSensor.disable_gesture();
+  //atum8::opticalSensor.set_led_pwm(100);
+  //atum8::opticalSensor.disable_gesture();
 
   // Calibrate Inertial Sensors
-  atum8::imuSensorAlpha.reset();
-  atum8::imuSensorBeta.reset();
-  atum8::imuSensorCharlie.reset();
-  while (atum8::imuSensorAlpha.is_calibrating() ||
-         atum8::imuSensorBeta.is_calibrating() ||
-         atum8::imuSensorCharlie.is_calibrating()) {
-    pros::lcd::set_text(1, "IMUs ARE CALIBRATING DON'T TOUCH!!!");
-    pros::delay(100);
-  }
+  //atum8::imuSensorAlpha.reset();
+  //atum8::imuSensorBeta.reset();
+  //atum8::imuSensorCharlie.reset();
+  // while (atum8::imuSensorAlpha.is_calibrating() ||
+  //        atum8::imuSensorBeta.is_calibrating() ||
+  //        atum8::imuSensorCharlie.is_calibrating()) {
+  //   pros::lcd::set_text(1, "IMUs ARE CALIBRATING DON'T TOUCH!!!");
+  //   pros::delay(100);
+  // }
 }
 
 void disabled() {}
@@ -43,7 +46,49 @@ void opcontrol() {
 		atum8::Drive drive;
     atum8::Vision vision;
 
-    vision.redAimBot();
+
+    while (true) {
+        pros::screen::set_pen(COLOR_RED);
+        pros::screen::fill_rect(0,0,240,120);
+
+        pros::screen::set_pen(COLOR_BLUE);
+        pros::screen::fill_rect(241,0,480,120);
+
+
+        pros::screen::set_pen(COLOR_PURPLE);
+        pros::screen::fill_rect(0,121,240,240);
+
+        pros::screen::set_pen(COLOR_DARK_ORANGE);
+        pros::screen::fill_rect(241,121,480,240);
+        
+        pros::screen::set_pen(COLOR_GREEN);
+
+        pros::screen_touch_status_s_t status = pros::screen::touch_status();
+
+        if(status.x >= 0 && status.y >= 0 && status.x <= 240 && status.y <= 120) {
+          std::cout << "Red" << std::endl;
+          pros::screen::fill_rect(0 + 50,0 + 40,240 - 50,120 - 40);
+        }
+        else if(status.x >= 241 && status.y >= 0 && status.x <= 480 && status.y <= 120) {
+          std::cout << "Blue" << std::endl;
+          pros::screen::fill_rect(241 + 50,0 + 40,480 - 50,120 - 40);
+        }
+        else if(status.x >= 0 && status.y >= 121 && status.x <= 240 && status.y <= 240) {
+          std::cout << "Purple" << std::endl;
+          pros::screen::fill_rect(0 + 50,121 + 40,240 - 50,240 - 40);
+        }
+        else if(status.x >= 241 && status.y >= 121 && status.x <= 480 && status.y <= 240) {
+          std::cout << "Orange" << std::endl;
+          pros::screen::fill_rect(241 + 50,121 + 40,480 - 50,240 - 40);
+        }
+ 
+
+pros::delay(100);
+
+    }
+
+
+    //vision.redAimBot();
 
 	//drive.move(24, 200, 600, false, 1);
 	//drive.move(-10, 200, 600, false, 1);
