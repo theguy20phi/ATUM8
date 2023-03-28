b0de9966c61998bccf6162a0218de67dd2e20e8a
thomasdang92 <thomasdang92@gmail.com>
2023-03-28 00:10:59 -0500

    Testing odometry.

--- 15in Worlds/src/main.cpp
+++ 15in Worlds/src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "atum8/odometry.hpp"
 #include "atum8/sensors/vision.hpp"
 #include "main.h"
 //#include "atum8\autonSelector.hpp"
@@ -8,14 +9,15 @@
 #include "atum8\globals.hpp"
 #include "atum8\systems\intake.hpp"
 #include "pros/colors.h"
+#include "pros/llemu.hpp"
 #include "pros/rtos.hpp"
 #include "pros/screen.hpp"
 
 
 void initialize() {
-	  //pros::lcd::initialize();
-  //pros::lcd::set_background_color(0, 0, 0);
-  //pros::lcd::set_text_color(255, 255, 255);
+  pros::lcd::initialize();
+  pros::lcd::set_background_color(0, 0, 0);
+  pros::lcd::set_text_color(255, 255, 255);
 
   // Enable Optical Sensors
   //atum8::opticalSensor.set_led_pwm(100);
@@ -43,55 +45,55 @@ void autonomous() {
 }
 
 void opcontrol() {
-		atum8::Drive drive;
-    atum8::Vision vision;
+  atum8::Odometry odometry;
+
+  std::string xString;
+  std::string yString;
+  
+  while(true) {
+    odometry.trackPosition();
+    xString =  atum8::globalX;
+    yString = atum8::globalY;
+    pros::lcd::set_text(1, xString);
+    pros::lcd::set_text(2, yString);
+  }
 
 
-    while (true) {
-        pros::screen::set_pen(COLOR_RED);
-        pros::screen::fill_rect(0,0,240,120);
+//     while (true) {
+//         pros::screen::set_pen(COLOR_RED);
+//         pros::screen::fill_rect(0,0,240,120);
 
-        pros::screen::set_pen(COLOR_BLUE);
-        pros::screen::fill_rect(241,0,480,120);
+//         pros::screen::set_pen(COLOR_BLUE);
+//         pros::screen::fill_rect(241,0,480,120);
 
 
-        pros::screen::set_pen(COLOR_PURPLE);
-        pros::screen::fill_rect(0,121,240,240);
+//         pros::screen::set_pen(COLOR_PURPLE);
+//         pros::screen::fill_rect(0,121,240,240);
 
-        pros::screen::set_pen(COLOR_DARK_ORANGE);
-        pros::screen::fill_rect(241,121,480,240);
+//         pros::screen::set_pen(COLOR_DARK_ORANGE);
+//         pros::screen::fill_rect(241,121,480,240);
         
-        pros::screen::set_pen(COLOR_GREEN);
+//         pros::screen::set_pen(COLOR_GREEN);
 
-        pros::screen_touch_status_s_t status = pros::screen::touch_status();
+//         pros::screen_touch_status_s_t status = pros::screen::touch_status();
 
-        if(status.x >= 0 && status.y >= 0 && status.x <= 240 && status.y <= 120) {
-          std::cout << "Red" << std::endl;
-          pros::screen::fill_rect(0 + 50,0 + 40,240 - 50,120 - 40);
-        }
-        else if(status.x >= 241 && status.y >= 0 && status.x <= 480 && status.y <= 120) {
-          std::cout << "Blue" << std::endl;
-          pros::screen::fill_rect(241 + 50,0 + 40,480 - 50,120 - 40);
-        }
-        else if(status.x >= 0 && status.y >= 121 && status.x <= 240 && status.y <= 240) {
-          std::cout << "Purple" << std::endl;
-          pros::screen::fill_rect(0 + 50,121 + 40,240 - 50,240 - 40);
-        }
-        else if(status.x >= 241 && status.y >= 121 && status.x <= 480 && status.y <= 240) {
-          std::cout << "Orange" << std::endl;
-          pros::screen::fill_rect(241 + 50,121 + 40,480 - 50,240 - 40);
-        }
- 
+//         if(status.x >= 0 && status.y >= 0 && status.x <= 240 && status.y <= 120) {
+//           std::cout << "Red" << std::endl;
+//           pros::screen::fill_rect(0 + 50,0 + 40,240 - 50,120 - 40);
+//         }
+//         else if(status.x >= 241 && status.y >= 0 && status.x <= 480 && status.y <= 120) {
+//           std::cout << "Blue" << std::endl;
+//           pros::screen::fill_rect(241 + 50,0 + 40,480 - 50,120 - 40);
+//         }
+//         else if(status.x >= 0 && status.y >= 121 && status.x <= 240 && status.y <= 240) {
+//           std::cout << "Purple" << std::endl;
+//           pros::screen::fill_rect(0 + 50,121 + 40,240 - 50,240 - 40);
+//         }
+//         else if(status.x >= 241 && status.y >= 121 && status.x <= 480 && status.y <= 240) {
+//           std::cout << "Orange" << std::endl;
+//           pros::screen::fill_rect(241 + 50,121 + 40,480 - 50,240 - 40);
+//         }
+//         pros::delay(100);
+// }
 
-pros::delay(100);
-
-    }
-
-
-    //vision.redAimBot();
-
-	//drive.move(24, 200, 600, false, 1);
-	//drive.move(-10, 200, 600, false, 1);
-	//drive.move(-5, 200, 600, false, 1);
-	//drive.turn(90, 200, 600, 1);
 }
