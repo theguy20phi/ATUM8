f6b88c90372824ab8234721b6a04b8141ceb9e01
thomasdang92 <thomasdang92@gmail.com>
2023-03-30 23:56:24 -0500

    Reorganize and imported auto selector.

--- 15in Worlds/include/atum8/autonSelector.hpp
+++ 15in Worlds/include/atum8/autonSelector.hpp
@@ -2,8 +2,8 @@
  * @file  autonSelector.hpp
  * @author Thomas Tran Dang (thomasdang92@gmail.com)
  * @brief This file provides the class for the autonomous selector on the V5 Brain.
- * @version 0.1
- * @date 2023-03-21
+ * @version 0.2
+ * @date 2023-03-30
  * 
  * @copyright Copyright (c) 2023
  * 
@@ -16,6 +16,28 @@
 namespace atum8 {
 class AutonSelector {
 public:
-    void displayScreen();
+  void selector();
+
+private:
+  void scrollRight();
+  void scrollLeft();
+  void confirmSelection();
+
+  void displayRedSelection();
+  void displayRedActive();
+
+  void displayBlueSelection();
+  void displayBlueActive();
+
+  void displaySkillsSelection();
+  void displaySkillsActive();
+
+  void displayTestingSelection();
+  void displayTestingActive();
+
+
+  short const int lcdMin{1};
+  short const int lcdMax{4};
+  short int lcdCurrent{1};
 };
-}
+} // namespace atum8
--- 15in Worlds/src/main.cpp
+++ 15in Worlds/src/main.cpp
@@ -2,17 +2,12 @@
 #include "atum8/algorithms/odometry.hpp"
 #include "atum8/sensors/imus.hpp"
 #include "atum8/sensors/vision.hpp"
-#include "main.h"
 //#include "atum8\autonSelector.hpp"
 #include "atum8\systems\catapult.hpp"
 #include "atum8\systems\drive.hpp"
 #include "atum8\systems\endGame.hpp"
 #include "atum8\globals.hpp"
 #include "atum8\systems\intake.hpp"
-#include "pros/colors.h"
-#include "pros/llemu.hpp"
-#include "pros/rtos.hpp"
-#include "pros/screen.hpp"
 
 atum8::Drive drive;
 atum8::Intake intake;
@@ -40,46 +35,4 @@ void autonomous() {
 void opcontrol() {
   odometry.setStartingPosition(0, 0, 0);
   odometry.start();
-  //drive.start();
-  //intake.start();
-  //drive.stop();
-  //intake.start();
-
-//     while (true) {
-//         pros::screen::set_pen(COLOR_RED);
-//         pros::screen::fill_rect(0,0,240,120);
-
-//         pros::screen::set_pen(COLOR_BLUE);
-//         pros::screen::fill_rect(241,0,480,120);
-
-
-//         pros::screen::set_pen(COLOR_PURPLE);
-//         pros::screen::fill_rect(0,121,240,240);
-
-//         pros::screen::set_pen(COLOR_DARK_ORANGE);
-//         pros::screen::fill_rect(241,121,480,240);
-        
-//         pros::screen::set_pen(COLOR_GREEN);
-
-//         pros::screen_touch_status_s_t status = pros::screen::touch_status();
-
-//         if(status.x >= 0 && status.y >= 0 && status.x <= 240 && status.y <= 120) {
-//           std::cout << "Red" << std::endl;
-//           pros::screen::fill_rect(0 + 50,0 + 40,240 - 50,120 - 40);
-//         }
-//         else if(status.x >= 241 && status.y >= 0 && status.x <= 480 && status.y <= 120) {
-//           std::cout << "Blue" << std::endl;
-//           pros::screen::fill_rect(241 + 50,0 + 40,480 - 50,120 - 40);
-//         }
-//         else if(status.x >= 0 && status.y >= 121 && status.x <= 240 && status.y <= 240) {
-//           std::cout << "Purple" << std::endl;
-//           pros::screen::fill_rect(0 + 50,121 + 40,240 - 50,240 - 40);
-//         }
-//         else if(status.x >= 241 && status.y >= 121 && status.x <= 480 && status.y <= 240) {
-//           std::cout << "Orange" << std::endl;
-//           pros::screen::fill_rect(241 + 50,121 + 40,480 - 50,240 - 40);
-//         }
-//         pros::delay(100);
-// }
-
 }
