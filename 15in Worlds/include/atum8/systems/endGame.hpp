a778929e8381e81b57d7100e0f8b10e1daab7721
thomasdang92 <thomasdang92@gmail.com>
2023-03-30 23:01:58 -0500

    Created end game class.

---
+++ 15in Worlds/include/atum8/systems/endGame.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file endGame.hpp
+ * @author Thomas Tran Dang (thomasdang92@gmail.com)
+ * @brief This file provides a class for all of the end game methods. 
+ * @version 0.1
+ * @date 2023-03-30
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#pragma once
+#include "atum8/globals.hpp"
+#include "main.h"
+
+namespace atum8 {
+class EndGame {
+public:
+  void controller();
+  void shoot();
+  void retract();
+private:
+};
+} // namespace atum8
---
+++ 15in Worlds/src/atum8/systems/endGame.cpp
@@ -0,0 +1,21 @@
+#include "atum8/systems/endGame.hpp"
+#include "main.h"
+
+namespace atum8 {
+void EndGame::controller() { 
+    if (Chris.get_digital(DIGITAL_UP))
+        shoot();
+    else if(Chris.get_digital(DIGITAL_DOWN))
+        retract();
+}
+
+void EndGame::shoot() {
+  endGame.set_value(true);
+  std::cout << "shooting" << std::endl;
+}
+
+void EndGame::retract() {
+  endGame.set_value(false);
+  std::cout << "retracting" << std::endl;
+}
+} // namespace atum8
